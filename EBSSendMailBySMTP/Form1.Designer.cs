d50bba6db564af025260dfd4291b6b6861c654d4
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2022-01-04 09:05:32 -0500

    Add files via upload

---
+++ EBSSendMailBySMTP/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace EBSSendMailBySMTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtsub = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bodeytxt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yolla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtsub
+            // 
+            this.txtsub.Location = new System.Drawing.Point(61, 70);
+            this.txtsub.Name = "txtsub";
+            this.txtsub.Size = new System.Drawing.Size(138, 20);
+            this.txtsub.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mesaj Başlığı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mail İçeriği";
+            // 
+            // bodeytxt
+            // 
+            this.bodeytxt.Location = new System.Drawing.Point(205, 70);
+            this.bodeytxt.Name = "bodeytxt";
+            this.bodeytxt.Size = new System.Drawing.Size(138, 20);
+            this.bodeytxt.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Dosya Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 212);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bodeytxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsub);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mail Yollama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtsub;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox bodeytxt;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EBSSendMailBySMTP/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace EBSSendMailBySMTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog Op; string Dosyayolu = ""; Attachment attachment;
+        string[] KullanıcıBilgileri = { "admin@admin.com", "passwd", "ebubekirbastama.com.tr", "587", "info@admin.com", "info@admin.com" };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EBSSmtpSender(txtsub.Text, bodeytxt.Text);
+        }
+
+        //Mesaj Yollama Tetodu
+        public void EBSSmtpSender(string subject, string body)
+        {
+
+            SmtpClient client = new SmtpClient();
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            client.Host = KullanıcıBilgileri[2].ToString();
+            client.Port = int.Parse(KullanıcıBilgileri[3]);
+
+            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(KullanıcıBilgileri[0].ToString(), KullanıcıBilgileri[1].ToString());
+            client.UseDefaultCredentials = false;
+            client.Credentials = credentials;
+
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress(KullanıcıBilgileri[4].ToString());
+            msg.To.Add(new MailAddress(KullanıcıBilgileri[5].ToString()));
+            if (Dosyayolu != "")// Eğer Dosya Seçildiyse çalışacak kod bloğu
+            {
+                attachment = new Attachment(Dosyayolu.ToString());
+                msg.Attachments.Add(attachment);
+                msg.Subject = subject;
+                msg.IsBodyHtml = true;
+                msg.Body = string.Format(body);
+                client.Send(msg);
+            }
+            else// Eğer Dosya Seçilmediyse çalışacak kod bloğu...
+            {
+                msg.Subject = subject;
+                msg.IsBodyHtml = true;
+                msg.Body = string.Format(body);
+                client.Send(msg);
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Op = new OpenFileDialog();
+                if (Op.ShowDialog()==DialogResult.OK)
+                {
+                    Dosyayolu = Op.FileName.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"EBSTime");
+            }
+        }
+    }
+}
